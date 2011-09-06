eb8f9ed66501f393b8eb60473be75bf2e48148f4
Matt D <matt@matt.tm>
2011-09-06 22:28:09 +0200

    Added alternative installation for rbenv

---
+++ mac/rbenv.sh
@@ -0,0 +1,52 @@
+# Attempt to load .profile
+[[ -s "$HOME/.profile" ]] && source ~/.profile
+
+# Install rbenv
+[[ ! -s "$HOME/.rbenv" ]] && git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
+
+# Install ruby-build
+[[ ! -s "$HOME/.ruby-build" ]] && git clone git://github.com/sstephenson/ruby-build.git ~/.ruby-build
+
+# Install Pow if it hasn't been installed yet
+[[ ! -s "$HOME/.pow" ]] && curl get.pow.cx | sh
+
+# Add rbenv to .profile and .powconfig
+if [[ ! $PATH == *rbenv* ]]
+then
+  echo "export PATH=\"$HOME/.rbenv/shims:$HOME/.rbenv/bin:\$PATH"\" >> ~/.profile
+  echo "export PATH=\"$HOME/.rbenv/shims:$HOME/.rbenv/bin:\$PATH"\" >> ~/.powconfig
+  `killall -HUP pow`
+fi
+
+# Add ruby-build to .profile
+if [[ ! $PATH == *ruby-build* ]]
+then
+  echo 'export PATH="$HOME/.ruby-build/bin:$PATH"' >> ~/.profile
+fi
+
+# Infer project name
+NAME=`basename \`pwd\``
+
+# Detect Ruby version
+VERSION=`~/.rbenv/bin/rbenv local`
+
+# Install Ruby if necessary
+[ $VERSION ] && [[ ! -s "$HOME/.rbenv/versions/$VERSION" ]] && ~/.ruby-build/bin/ruby-build $VERSION ~/.rbenv/versions/$VERSION/ && ~/.rbenv/bin/rbenv rehash
+
+# Install bundler
+[[ ! -s "$HOME/.rbenv/shims/bundle" ]] && ~/.rbenv/bin/rbenv exec gem install bundler && ~/.rbenv/bin/rbenv rehash
+
+# Install gems
+~/.rbenv/bin/rbenv exec bundle check || ~/.rbenv/bin/rbenv exec bundle install --without=production
+
+# Copy database config
+[[ ! -s "config/database.yml" ]] && cp config/database.yml{.example,}
+
+# Run project setup
+~/.rbenv/bin/rbenv exec bundle exec rake db:setup
+
+# Add to Pow
+[[ ! -s "$HOME/.pow/$NAME" ]] && ln -s "`pwd`" ~/.pow/
+
+# Open project in default browser
+open http://$NAME.dev
